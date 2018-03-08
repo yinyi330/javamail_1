01f8dfca81e3ccb536e5f3dcd10a11a478d1f1cd
yinyi330 <yy18683783118@163.com>
2018-03-08 15:46:55 +0800

    first commit

---
+++ src/main/java/com/yy/test/TestLog4j2.java
@@ -0,0 +1,23 @@
+package com.yy.test;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by Administrator on 2018/3/8/008.
+ */
+public class TestLog4j2 {
+    static Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+
+    public static void main(String[] args) {
+        logger.trace("trace message");
+        logger.debug("debug message");
+        logger.info("info message");
+        logger.warn("warn message");
+        logger.error("error message");
+        logger.fatal("fatal message");
+        System.out.println("Hello World!");
+    }
+
+}
